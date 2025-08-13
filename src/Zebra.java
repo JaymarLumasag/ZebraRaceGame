0189de0b1992b05067f73dbdd1eee253031ab40c
davenatsirt <davenatsirt@gmail.com>
2025-08-13 17:15:56 +0800

    [08/13/2025] - Implement overall user input feature

--- src/Zebra.java
+++ src/Zebra.java
@@ -2,31 +2,37 @@ public class Zebra{
     private String zebraName;
     private int zebPosTrack;
 
-    //Zebra Constructor
+    // Zebra Constructor with name and position arguments
     public Zebra(String name,int positionTrack){
         setZebraName(name);
         setZebPosTrack(positionTrack);
     }
 
+    // Method for the zebra to increment the current position
     public void moveForward(){
+
         ++this.zebPosTrack;
     }
 
     //setters
     public void setZebraName(String zebraName) {
+
         this.zebraName = zebraName;
     }
 
     public void setZebPosTrack(int zebPosTrack) {
+
         this.zebPosTrack = zebPosTrack;
     }
 
     //getters
     public String getZebraName() {
+
         return zebraName;
     }
 
     public int getZebPosTrack() {
+
         return zebPosTrack;
     }
 }
